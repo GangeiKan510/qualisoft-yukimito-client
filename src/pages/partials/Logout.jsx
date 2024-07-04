8d9bd557f13a85114436f61569279df191729b66
theblurrysean <79564574+GangeiKan510@users.noreply.github.com>
2024-07-04 21:05:17 +0800

    feat: added a dialog for logout confirmation

--- src/pages/partials/Logout.jsx
+++ src/pages/partials/Logout.jsx
@@ -1,18 +1,54 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import LogoutIcon from '@mui/icons-material/Logout';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 export default function Logout(props) {
-
   const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
 
-  function handleLogout () {
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleLogout = () => {
     localStorage.removeItem('token');
     navigate(props.link);
-  }
+  };
 
   return (
     <>
-      <a onClick={handleLogout} class="nav-link text-white" href={props.link}><LogoutIcon className="me-1"/>Log out</a>
+      <a onClick={handleClickOpen} className="nav-link text-white" href="#"><LogoutIcon className="me-1"/>Log out</a>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Confirm Logout"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to log out?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <button className="btn btn-outline-warning" onClick={handleClose} color="primary">
+            Cancel
+          </button>
+          <button className="btn btn-danger" onClick={handleLogout} color="primary" autoFocus>
+            Logout
+          </button>
+        </DialogActions>
+      </Dialog>
     </>
-  )
-};
+  );
+}
